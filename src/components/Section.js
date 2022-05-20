b38c77b66e48efbb9c4c5065f8f9b3d6a6372133
Mounika-Krish <mounikak600@gmail.com>
2022-05-20 14:17:35 +0530

    final commit

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import Fade from 'react-reveal/Fade';
 
 function Section(props) {
   return (
     <Wrap bgImage={props.backgroundImg}>
+      <Fade bottom>
       <ItemText>
         <h1>{props.title}</h1>
         <p>{props.description}</p>
       </ItemText>
+      </Fade>
+
       <Buttons>
+        <Fade bottom>
         <ButtonGroup>
           <LeftButton>
           {props.leftBtnText}
@@ -21,8 +26,10 @@ function Section(props) {
           }
           
         </ButtonGroup>
+        </Fade>
       <DownArrow src="/images/down-arrow.svg"/>
       </Buttons>
+      
     </Wrap>
   )
 }
